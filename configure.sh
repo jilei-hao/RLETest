e8707fbd5c27acc4e055a62e3ef7d7ab6db5ff1b
Jilei Hao <71778110+jilei-hao@users.noreply.github.com>
2024-03-26 22:49:35 -0400

    Delete configure.sh

--- configure.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cmake -S /Users/jileihao/dev/sandbox/RLETest -B /Users/jileihao/dev/sandbox/RLETest/build \
--D CMAKE_BUILD_TYPE=Debug \
--D ITK_DIR=/Users/jileihao/dev/itk-dev/build-debug-dynamic

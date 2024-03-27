2ed3855db4efe1586845aa98c6738f39ddeacc01
jilei-hao <jilei.cs@gmail.com>
2024-03-26 22:54:43 -0400

    Update RLEIteratorTest.cxx

--- RLEIteratorTest.cxx
+++ RLEIteratorTest.cxx
@@ -6,11 +6,11 @@ int main (int argc, char *argv[])
 {
   // works
   const itk::Image<float, 3> *constFloat = itk::Image<float, 3>::New();
-  itk::ImageScanlineConstIterator it(constFloat, constFloat->GetLargestPossibleRegion());
+  itk::ImageScanlineConstIterator it1(constFloat, constFloat->GetLargestPossibleRegion());
 
   // complile error
   const shortRLEImage *constRLE = itkRLEImage<short, 3>::New();
-  itk::ImageScanlineConstIterator it(constRLE, constRLE->GetLargestPossibleRegion());
+  itk::ImageScanlineConstIterator it2(constRLE, constRLE->GetLargestPossibleRegion());
 
   return 0;
 }
